f7b953b0a3e70dafb0bf381b40647a7fc47705df
ReC <kitanin.ser@mail.ru>
2019-03-22 10:20:13 +0300

    Add icons to ios project. Need - launch image

--- Http_Post/Http_Post.iOS/Properties/AssemblyInfo.cs
+++ Http_Post/Http_Post.iOS/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Http_Post.iOS")]
+[assembly: AssemblyTitle("ITLab-Mobile")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Http_Post.iOS")]
+[assembly: AssemblyProduct("ITLab-Mobile")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
